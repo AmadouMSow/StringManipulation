f983cdfecb150f2cd98fa0457db3097ad0b4a581
RC_Student_lab <RC_Student_lab@10.0.0.252>
2024-09-12 08:52:53 +0000

    -Second commit Ice Task 6 (1)

--- src/main/java/com/mycompany/icetask6/stringmanipulationandgithubpush/VowelCount.java
+++ src/main/java/com/mycompany/icetask6/stringmanipulationandgithubpush/VowelCount.java
@@ -26,19 +26,17 @@ public class VowelCount {
     }
 
     public static void main(String[] args) {
-        // Create a Scanner object to take input from the user
-        Scanner scanner = new Scanner(System.in);
-        
         // Ask lthe user to input a string
-        System.out.println("Please enter a sentence:");
-        String userInput = scanner.nextLine();
-        
-        // Call the countVowels function and print the result
-        int numberOfVowels = countVowels(userInput);
-        System.out.println("Number of vowels in the given sentence: " + numberOfVowels);
-        
-        // Close the scanner
-        scanner.close();
+        try ( // Create a Scanner object to take input from the user
+                Scanner scanner = new Scanner(System.in)) {
+            // Ask lthe user to input a string
+            System.out.println("Please enter a sentence:");
+            String userInput = scanner.nextLine();
+            // Call the countVowels function and print the result
+            int numberOfVowels = countVowels(userInput);
+            System.out.println("Number of vowels in the given sentence: " + numberOfVowels);
+            // Close the scanner
+        }
     }
 }
 /*
